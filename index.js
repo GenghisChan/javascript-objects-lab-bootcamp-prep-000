b4bec83ec53fb37c89f51c65bd63b3ca65b0a583
Christofer Chan <devchumbing@gmail.com>
2018-05-17 20:18:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function updateObjectWithKeyAndValue(object,key,value){
 }
 
 function updateObjectWithKeyAndValue(object, key, value){
-  Object.assign({}, object, object[key] = value)
-  return newObj
+  return Object.assign({}, object)
+  
 }
